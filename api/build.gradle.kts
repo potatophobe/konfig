a6da5b617f08d3f4c6968b1dfdf632f289146b61
potatophobe <potatophobe@gmail.com>
2022-07-17 19:28:30 +0300

    Configure gradle

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,19 @@
+val projectRevision: String by project
+
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 group = "ru.potatophobe.konfig"
+version = projectRevision
+
+dependencies {
+    api(kotlin("reflect"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") { from(components["java"]) }
+    }
+}
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,16 +1,20 @@
+val projectRevision: String by project
+
 plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp")
 }
 
 group = "ru.potatophobe.konfig"
+version = projectRevision
 
 dependencies {
+    ksp(project(":ksp"))
     implementation(project(":kts"))
-
-    testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+kotlin {
+    sourceSets.main {
+        kotlin.srcDir("build/generated/ksp/main/kotlin")
+    }
 }
