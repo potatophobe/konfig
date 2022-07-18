87bf2ddfdb1d844b761759476bd1422a57ac6673
potatophobe <potatophobe@gmail.com>
2022-07-18 18:22:13 +0300

    Configure maven publishing

--- full/build.gradle.kts
+++ full/build.gradle.kts
@@ -3,6 +3,7 @@ val projectRevision: String by project
 plugins {
     `java-platform`
     `maven-publish`
+    signing
 }
 
 group = "ru.potatophobe.konfig"
@@ -21,6 +22,50 @@ dependencies {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") { from(components["javaPlatform"]) }
+        create<MavenPublication>("maven") {
+            from(components["javaPlatform"])
+            pom {
+                name.set("Konfig Full")
+                description.set("Full set of Konfig modules")
+                url.set("https://github.com/potatophobe/konfig")
+                developers {
+                    developer {
+                        id.set("potatophobe")
+                        name.set("Artem Stroev")
+                        email.set("potatophobe@gmail.com")
+                    }
+                }
+                licenses {
+                    license {
+                        name.set("Apache License Version 2.0")
+                        url.set("https://raw.githubusercontent.com/potatophobe/konfig/master/LICENSE")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/potatophobe/konfig")
+                    connection.set("scm:git:https://github.com/potatophobe/konfig.git")
+                    developerConnection.set("scm:git:https://github.com/potatophobe/konfig.git")
+                }
+            }
+            repositories {
+                maven {
+                    val ossrhUsername: String by project
+                    val ossrhPassword: String by project
+
+                    url = run {
+                        if (projectRevision.endsWith("SNAPSHOT")) uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                        else uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    }
+                    credentials {
+                        username = ossrhUsername
+                        password = ossrhPassword
+                    }
+                }
+            }
+        }
     }
+}
+
+signing {
+    sign(publishing.publications["maven"])
 }
